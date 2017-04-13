1a9ac690b104c5370145672c1eef3ecd37bc2c59
Michael Palmer <michael@michaeldpalmer.com>
2017-04-13 08:03:14 -0400

    starting app

---
+++ StarGazer/app/src/main/java/com/michaelpalmer/stargazer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.michaelpalmer.stargazer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
